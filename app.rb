e9cb16fb063d146cf3968d3b1c3d94ba3b935c7a
csergey88 <s.chornyy8@gmail.com>
2021-02-14 12:50:15 +0200

    commit

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ require './helpers'
 
 
 configure do
-    db = Mongo::Client.new([ 'ruby-db:27107' || '127.0.0.1:27017' ], database: 'user_posts', heartbeat_frequency: 2)
+    db = Mongo::Client.new(['ruby-db:27107'], database: 'user_posts', heartbeat_frequency: 2)
     set :mongo_db, db[:posts]
     set :comments_db, db[:comments]
     set :users_db, db[:users]
